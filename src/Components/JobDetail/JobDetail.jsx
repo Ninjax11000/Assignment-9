0fc00c8dd1eefb66d84585875c12485aa12b2429
Ninjax11000 <jamiljoy007@gmail.com>
2023-04-11 13:42:35 +0600

    fixed job details layout

--- src/Components/JobDetail/JobDetail.jsx
+++ src/Components/JobDetail/JobDetail.jsx
@@ -35,56 +35,61 @@ const JobDetail = () => {
             </div>
 
 
-            <div className='flex'>
+            <div className='md:flex my-8 py-8 mx-8 px-8'>
                 {/* description */}
-                <div className='w-2/3'>
-                    {/* <p><span>Job Description: </span>{description}</p>
-                    <p><span>Job Responsibility:</span>{responsibility}</p>
-                    <h2>Educational Requirements:</h2>
+                <div className='w-2/3 my-2'>
+                    <p className='my-2'><span className='font-bold' >Job Description: </span>{description}</p>
+                    <p className='my-2'><span className='font-bold'>Job Responsibility:</span>{responsibility}</p>
+                    <h2 className='font-bold my-3'>Educational Requirements:</h2>
                     <p>{educational_Requirements}</p>
-                    <h2>Experiences:</h2>
-                    <p>{experiences}</p> */}
+                    <h2 className='font-bold my-3'>Experiences:</h2>
+                    <p>{experiences}</p>
 
                 </div>
 
                 {/* card */}
                 <div>
-                    <div>
-                        <h2>Job Details</h2>
-                        <hr />
+                <div className='bg-slate-100 rounded-lg'>
+                    <div className='mx-4'>
+                        <h2 className='text-lg font-bold py-4'>Job Details</h2>
+                        <hr className='border-1 border-gray-600' />
                     </div>
-                    <div>
-                        <div className='flex'>
-                            <img className='w-4 h-5' src={sPic} alt="" />
-                            <p><span>Salary:</span> {salary} Per Month</p>
+                    <div className='py-4 mx-4'>
+                        <div className='flex gap-1 items-center my-2'>
+                            <img className='w-5 h-5' src={sPic} alt="" />
+                            <p className='text-lg'><span className='font-bold text-slate-600'>Salary:</span> {salary} Per Month</p>
                         </div>
-                        <div className='flex'>
-                            <img className='w-4 h-5' src={jPic} alt="" />
-                            <p><span>Job Title: </span>{title}</p>
+                        <div className='flex gap-1 items-center'>
+                            <img className='w-5 h-5' src={jPic} alt="" />
+                            <p className='text-lg'><span className='font-bold text-slate-600'>Job Title: </span>{title}</p>
                         </div>
                     </div>
-                    <div>
-                        <h2>Contact Information</h2>
-                        <hr />
+                    <div className='mx-4 mb-2'>
+                        <h2 className='text-lg font-bold pb-3'>Contact Information</h2>
+                        <hr  className='border-1 border-gray-600'/>
                     </div>
-                    <div>
+                    <div className='mx-4 pb-6 my-2 '>
                         {/* phone */}
-                        <div className='flex'>
-                            <img className='w-4 h-5' src={PPic} alt="" />
-                            <p><span>Phone: </span>{phone}</p>
+                        <div className='flex gap-1 mb-2 items-center'>
+                            <img className='w-5 h-5' src={PPic} alt="" />
+                            <p className='text-lg'><span className='font-bold text-slate-600'>Phone: </span>{phone}</p>
                         </div>
                         {/* email */}
-                        <div className='flex'>
-                            <img className='w-4 h-5' src={EPic} alt="" />
-                            <p><span>Email: </span>{email}</p>
+                        <div className='flex gap-1 mb-2 items-center'>
+                            <img className='w-5 h-5' src={EPic} alt="" />
+                            <p className='text-lg'><span className='font-bold text-slate-600'>Email: </span>{email}</p>
                         </div>
                         {/* address */}
-                        <div className='flex'>
-                            <img className='w-4 h-5' src={APic} alt="" />
-                            <p><span>Address: </span>{location}</p>
+                        <div className='flex gap-1 items-center'>
+                            <img className='w-5 h-5' src={APic} alt="" />
+                            <p className='text-lg'><span className='font-bold text-slate-600'>Address: </span>{location}</p>
                         </div>
                     </div>
+                    
                 </div>
+                <button className='bg-indigo-500 rounded-md py-2 px-2 mt-2 text-white font-bold text-lg w-full'>Apply now</button>
+                </div>
+                
 
             </div>
 
