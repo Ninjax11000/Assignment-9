6734eafa52825c27572d8e6a97877e60c331ef95
Ninjax11000 <jamiljoy007@gmail.com>
2023-04-11 13:14:37 +0600

    aaded side cart at job details page

--- src/Components/JobDetail/JobDetail.jsx
+++ src/Components/JobDetail/JobDetail.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 import banner from '../../assets/All Images/Vector.png';
 import sPic from '../../assets/Icons/Frame.png';
 import jPic from '../../assets/Icons/Frame-1.png';
+import PPic from '../../assets/Icons/Frame-2.png';
+import EPic from '../../assets/Icons/Frame-3.png';
+import APic from '../../assets/Icons/Frame-4.png';
+
 
 
 const JobDetail = () => {
@@ -19,6 +23,7 @@ const JobDetail = () => {
 
     const detail = details.find(detail => detail.id === lastsegment);
     console.log(detail);
+    const { description, responsibility, educational_Requirements, experiences, salary, title, phone, email, location } = detail;
 
     return (
         <div >
@@ -29,21 +34,61 @@ const JobDetail = () => {
 
             </div>
 
-            
-            <div>
+
+            <div className='flex'>
                 {/* description */}
-                <div>
-                    <p><span>Job Description: </span>{detail.description}</p>
+                <div className='w-2/3'>
+                    {/* <p><span>Job Description: </span>{description}</p>
+                    <p><span>Job Responsibility:</span>{responsibility}</p>
+                    <h2>Educational Requirements:</h2>
+                    <p>{educational_Requirements}</p>
+                    <h2>Experiences:</h2>
+                    <p>{experiences}</p> */}
+
                 </div>
 
                 {/* card */}
                 <div>
-
+                    <div>
+                        <h2>Job Details</h2>
+                        <hr />
+                    </div>
+                    <div>
+                        <div className='flex'>
+                            <img className='w-4 h-5' src={sPic} alt="" />
+                            <p><span>Salary:</span> {salary} Per Month</p>
+                        </div>
+                        <div className='flex'>
+                            <img className='w-4 h-5' src={jPic} alt="" />
+                            <p><span>Job Title: </span>{title}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <h2>Contact Information</h2>
+                        <hr />
+                    </div>
+                    <div>
+                        {/* phone */}
+                        <div className='flex'>
+                            <img className='w-4 h-5' src={PPic} alt="" />
+                            <p><span>Phone: </span>{phone}</p>
+                        </div>
+                        {/* email */}
+                        <div className='flex'>
+                            <img className='w-4 h-5' src={EPic} alt="" />
+                            <p><span>Email: </span>{email}</p>
+                        </div>
+                        {/* address */}
+                        <div className='flex'>
+                            <img className='w-4 h-5' src={APic} alt="" />
+                            <p><span>Address: </span>{location}</p>
+                        </div>
+                    </div>
                 </div>
 
             </div>
 
-          
+
 
 
         </div>
